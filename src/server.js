a8e160d3fbcc943212a1aec5e280dd8418b3b491
Nicholas Gunter <ygnick@live.com>
2019-09-06 16:59:32 -0400

    added data to dog and cat array

--- src/server.js
+++ src/server.js
@@ -43,7 +43,25 @@ const catsData = [
     age: 2,
     breed: 'Bengal',
     story: 'Thrown on the street'
-  }
+  },
+  {
+    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Kitten',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Birdie',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
 ];
 
 const dogsData = [
@@ -55,7 +73,25 @@ const dogsData = [
     age: 3,
     breed: 'Golden Retriever',
     story: 'Owner Passed away'
-  }
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Bubba',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Gus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
 ];
 
 for (let i = 0; i < catsData.length; i++) {
